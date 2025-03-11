1ca77d3a0377f90b869cadeaa5d72fdf7051429b
Anindya Lokeswara <anindya.lokeswara@ui.ac.id>
2025-03-12 03:39:08 +0700

    chore: change fogverse library directory

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
-       "fogverse @ git+https://github.com/FXDROS/fogverse.git@main#egg=fogverse"
+       "fogverse @ git+https://github.com/naufalweise/fogverse.git@refactoring#egg=fogverse"
     ],
     url="https://github.com/anindyalkwr/log-sdk.git",
     classifiers=[
